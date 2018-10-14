e10ed9c4cb2e0eb429e85eb64aec96f55d8032b0
daynaeidle <dayna.eidle1@marist.edu>
2018-10-14 10:25:43 -0400

    Wrote all programs for each case of op code statement. Need to test them out. Still not positive on FF or D0.

--- source/host/cpu.ts
+++ source/host/cpu.ts
@@ -42,45 +42,120 @@ module TSOS {
             // TODO: Accumulate CPU usage and profiling statistics here.
             // Do the real work here. Be sure to set this.isExecuting appropriately.
 
-            var opcode = fetch();
+            var opcode = this.fetch(this.PC);
         }
 
-        public fetch(pid: number) {
+        public fetch(memAddress: number) {
+            this.isExecuting = true;
             //fetch an instruction to decode from a process
+            return _MemoryAccessor.readMemory(memAddress);
 
         }
 
         public decode(opcode: string){
             //find out what the instruction means
 
+            var val;
+            var address;
+
             switch(opcode){
+
                 case("A9"):
+                    //load the accumulator with a constant
+                    val = this.fetch(this.PC + 1);
+                    this.Acc = val;
+                    this.PC += 2;
                     break;
                 case("AD"):
+                    //load the accumulator from memory
+                    address = parseInt((this.fetch(this.PC+2).toString(), this.fetch(this.PC+1).toString()));
+                    val = this.fetch(address);
+                    this.Acc = val;
+                    this.PC += 3;
                     break;
                 case ("8D"):
+                    //store the accumulator in memory
+                    val = this.Acc;
+                    address = parseInt((this.fetch(this.PC+2).toString(), this.fetch(this.PC+1).toString()));
+                    _MemoryAccessor.writeValue(address, val);
+                    this.PC += 3;
                     break;
                 case("6D"):
+                    //add with carry
+                    address = parseInt((this.fetch(this.PC+2).toString(), this.fetch(this.PC+1).toString()));
+                    val = this.fetch(address);
+                    this.Acc = this.Acc + val;
+                    this.PC += 3;
                     break;
                 case("A2"):
+                    //load x register with a constant
+                    val = this.fetch(this.PC + 1);
+                    this.Xreg = val;
+                    this.PC += 2;
                     break;
                 case("AE"):
+                    //load x register from memory
+                    address = address = parseInt((this.fetch(this.PC+2).toString(), this.fetch(this.PC+1).toString()));
+                    val = this.fetch(address);
+                    this.Xreg = val;
+                    this.PC += 3;
                     break;
                 case("A0"):
+                    //load y register with a constant
+                    val = this.fetch(this.PC + 1);
+                    this.Yreg = val;
+                    this.PC += 2;
                     break;
                 case("AC"):
+                    //load y register from memory
+                    address = address = parseInt((this.fetch(this.PC+2).toString(), this.fetch(this.PC+1).toString()));
+                    val = this.fetch(address);
+                    this.Yreg = val;
+                    this.PC += 3;
                     break;
                 case("EA"):
+                    //do nothing
+                    this.PC += 1;
                     break;
                 case("00"):
+                    //break
+                    this.isExecuting = false;
                     break;
                 case("EC"):
+                    //compares a byte in memory to the xreg - changes zflag if equal
+                    address = address = parseInt((this.fetch(this.PC+2).toString(), this.fetch(this.PC+1).toString()));
+                    val = this.fetch(address);
+                    if (this.Xreg == val){
+                        this.Zflag = 1;
+                    }else{
+                        this.Zflag = 0;
+                    }
                     break;
                 case("D0"):
+                    //branch n bytes if zflag = 0
+                    if (this.Zflag == 0){
+                        this.PC = = this.fetch(this.PC + 1);
+                        this.PC += 2;
+                    }else{
+                        this.PC += 2;
+                    }
                     break;
                 case("EE"):
+                    //incrememnt the value of a byte
+                    address = address = parseInt((this.fetch(this.PC+2).toString(), this.fetch(this.PC+1).toString()));
+                    val = this.fetch(address);
+                    _MemoryAccessor.writeValue(address, val + 1);
+                    this.PC += 3;
                     break;
                 case("FF"):
+                    //system call
+                    //01 in xregprint the integer stored in the y register
+                    //02 in xreg print the 00 terminated string stored at the y register
+                    if (this.Xreg == 1){
+                        console.log(this.Yreg);
+                    }else if (this.Xreg == 2){
+                        console.log((this.Yreg).toString);
+                    }
                     break;
                 default:
                     console.log("Not a valid op code");
--- source/host/memoryAccessor.ts
+++ source/host/memoryAccessor.ts
@@ -3,11 +3,39 @@ module TSOS {
 
     export class MemoryAccessor {
 
-        public readProcess(){
+        public readValue(address: number){
+
+            var base = _Pcb.PC;
+            var limit = 255;
+            //create memory address from base of process
+            var memAddress = base + address;
+
+            //check to see if memory address created is within the process bounds in memory
+            if (memAddress < (base + limit)){
+                //return value at address in memory
+                return _Memory.mainMem[memAddress];
+            }else{
+                console.log("Memory address out of bounds.");
+            }
+
+
 
         }
 
-        public writeProcess(){
+        public writeValue(address: number, value: number){
+
+            var base = _Pcb.PC;
+            var limit = 255;
+            //create memory address from base of process
+            var memAddress = base + address;
+
+            //check to see if memory address created is within the process bounds in memory
+            if (memAddress <= (base + limit)){
+                //set value at memory location
+                _Memory.mainMem[memAddress] = value;
+            }else{
+                console.log("Memory address out of bounds.");
+            }
 
         }
 
