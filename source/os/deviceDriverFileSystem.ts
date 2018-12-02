2c7be279f4f24d8593a14ac1d781568eeaf2cdd9
daynaeidle <dayna.eidle1@marist.edu>
2018-12-01 21:57:57 -0500

    Filled disk with 00 to start and started working on create file

--- source/os/deviceDriverFileSystem.ts
+++ source/os/deviceDriverFileSystem.ts
@@ -22,6 +22,62 @@ module TSOS {
             // Initialization routine for this, the kernel-mode Keyboard Device Driver.
             this.status = "loaded";
             // More?
+
+            if (sessionStorage){
+
+                var tsb: string;
+
+                var lineValue = [];
+
+                //set base line values to 00
+                for (var i = 0; i < this.blockSize; i++){
+                    lineValue.push("00");
+                }
+
+                //create tsb for each track sector block and store in session storage with linevalue
+                for (var i = 0; i < this.track; i++){
+                    for (var j = 0; j < this.sector; j++){
+                        for (var k = 0; k < this.block; j++){
+                            tsb = i.toString() + j.toString() + k.toString();
+                            sessionStorage.setItem(tsb, JSON.stringify(lineValue));
+                        }
+                    }
+                }
+
+            }else{
+
+                console.log("Sorry your browser does not support Session Storage.");
+            }
+
+
+        }
+
+
+
+        public createFile(filename){
+
+            var hexName: string = "";
+
+            for (var i = 0; i < filename.length; i++){
+
+                hexName += String(filename.charCodeAt(i).toString(16));
+            }
+
+        }
+
+
+        public writeFile(filename, str){
+
+        }
+
+
+        public readFile(filename){
+
+        }
+
+
+        public deleteFile(filename){
+
         }
 
     }
